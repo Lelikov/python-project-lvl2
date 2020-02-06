082d198005e5a51a9dc44689996e0240ce51d89b
Alexandr Lelikov <lelikovas@gmail.com>
2020-02-06 11:11:44 +0300

    Deleted type=int

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,9 +3,9 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file', type=int)
-    parser.add_argument('second_file', type=int)
-    parser.add_argument('-f', '--format', type=int)
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format')
     args = parser.parse_args()
 
 
