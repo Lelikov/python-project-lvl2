3a97ee0ad6884ab9f09cd352d15ab828f67e1987
Alexandr Lelikov <lelikovas@gmail.com>
2020-02-11 16:04:29 +0300

    Deleted blank lines

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -5,7 +5,6 @@ def generate_diff(path_to_file1, path_to_file2):
     before = json.load(open(path_to_file1))
     after = json.load(open(path_to_file2))
     array = ["{"]
-
     for key, value in before.items():
         if after.get(key) is not None:
             if before[key] == after[key]:
@@ -15,10 +14,8 @@ def generate_diff(path_to_file1, path_to_file2):
                 array.append("  - {}: {}".format(key, value))
         else:
             array.append("  - {}: {}".format(key, value))
-
     for key, value in after.items():
         if before.get(key) is None:
             array.append("  + {}: {}".format(key, value))
-
     array.append("}")
     return '\n'.join(array)
