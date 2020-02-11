2256be0c51365823a5fb95c44b53ffedb17e4078
Alexandr Lelikov <lelikovas@gmail.com>
2020-02-11 21:08:16 +0300

    Lintered

--- gendiff/parsers.py
+++ gendiff/parsers.py
@@ -1,10 +1,12 @@
 import json
 import yaml
 
+
 def parse_json(path_to_file1, path_to_file2):
     before = json.load(open(path_to_file1))
     after = json.load(open(path_to_file2))
     return before, after
+
 
 def parse_yaml(path_to_file1, path_to_file2):
     before = open(path_to_file1, "r")
--- tests/test_first.py
+++ tests/test_first.py
@@ -1,9 +1,11 @@
 from gendiff.engine import generate_diff
 
+
 def test_diff_json():
     f = open("tests/fixtures/result.txt", "r")
     assert generate_diff("tests/fixtures/before.json", "tests/fixtures/after.json") == f.read()
     f.close()
+
 
 def test_diff_yaml():
     f = open("tests/fixtures/result.txt", "r")
