11214f9e6a109a61c7fd7c61be8bdcfb766f00ed
Alexandr Lelikov <lelikovas@gmail.com>
2020-02-19 14:26:24 +0300

    Fix

--- tests/test_input_formats.py
+++ tests/test_input_formats.py
@@ -2,10 +2,10 @@ from gendiff.engine import generate_diff
 
 
 def test_input_formats():
-    with open("tests/fixtures/result_plain.txt", "r") as result_json:
+    with open("tests/fixtures/plain.golden", "r") as result_json:
         assert generate_diff("tests/fixtures/before.yml",
                              "tests/fixtures/after.yml", "plain") == result_json.read()
 
-    with open("tests/fixtures/result_plain.txt", "r") as result_plain:
+    with open("tests/fixtures/plain.golden", "r") as result_plain:
         assert generate_diff("tests/fixtures/before.json",
                              "tests/fixtures/after.json", "plain") == result_plain.read()
--- tests/test_output_formats.py
+++ tests/test_output_formats.py
@@ -2,18 +2,18 @@ from gendiff.engine import generate_diff
 
 
 def test_output_formats():
-    with open("tests/fixtures/result_plain.txt", "r") as result_plain:
+    with open("tests/fixtures/plain.golden", "r") as result_plain:
         assert generate_diff("tests/fixtures/before.json",
                              "tests/fixtures/after.json", "plain") == result_plain.read()
 
-    with open("tests/fixtures/result_json.txt", "r") as result_json:
+    with open("tests/fixtures/json.golden", "r") as result_json:
         assert generate_diff("tests/fixtures/before.json",
                              "tests/fixtures/after.json", "json") == result_json.read()
 
-    with open("tests/fixtures/result_text.txt", "r") as result_text:
+    with open("tests/fixtures/text.golden", "r") as result_text:
         assert generate_diff("tests/fixtures/before.json",
                              "tests/fixtures/after.json", "text") == result_text.read()
 
-    with open("tests/fixtures/result_plain_text.txt", "r") as result_plain_text:
+    with open("tests/fixtures/plain_text.golden", "r") as result_plain_text:
         assert generate_diff("tests/fixtures/before_plain.json",
                              "tests/fixtures/after_plain.json", "text") == result_plain_text.read()
