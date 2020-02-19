0200cf8f7fd78813bd536b84f01f31e59d2cc068
Alexandr Lelikov <lelikovas@gmail.com>
2020-02-19 13:55:40 +0300

    Added descriptions in README.md

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 <a href="https://travis-ci.org/Lelikov/python-project-lvl2"><img src="https://travis-ci.org/Lelikov/python-project-lvl2.svg?branch=master"></a>
 <a href="https://codeclimate.com/github/Lelikov/python-project-lvl2/maintainability"><img src="https://api.codeclimate.com/v1/badges/1e451f4c91cd28c3176c/maintainability" /></a>
+#**Difference generator**
+Utility for generating difference between two files.
+Support .json and .yml files
 ## Difference in text format
+```
+gendiff path_to_first_file path_to_second_file -f text
+```
 <a href="https://asciinema.org/a/5fEYkRZFLXVZRwuewZWwDMxyZ" target="_blank"><img src="https://asciinema.org/a/5fEYkRZFLXVZRwuewZWwDMxyZ.svg" /></a>
 ## Difference in plain format
+```
+gendiff path_to_first_file path_to_second_file -f plain
+```
 <a href="https://asciinema.org/a/lRTc6eyiUSh7GeT1OshguCOra" target="_blank"><img src="https://asciinema.org/a/lRTc6eyiUSh7GeT1OshguCOra.svg" /></a>
 ## Difference in JSON format
+```
+gendiff path_to_first_file path_to_second_file -f json
+```
 <a href="https://asciinema.org/a/3Q3vf12MuolsBuOQQGub1jaD7" target="_blank"><img src="https://asciinema.org/a/3Q3vf12MuolsBuOQQGub1jaD7.svg" /></a>
--- gendiff/formatters/text_render.py
+++ gendiff/formatters/text_render.py
@@ -2,6 +2,12 @@ from gendiff.constructors import dictionary_constructor
 import collections
 from gendiff.constants import ADDED, DELETED, CHANGED, NO_CHANGED
 
+CHANGE_SYMBOL = {
+    ADDED: '+',
+    DELETED: '-',
+    NO_CHANGED: ' ',
+    CHANGED: ''
+}
 
 def text_render(text_list):
     '''
@@ -10,11 +16,10 @@ def text_render(text_list):
     :return: Text
     '''
     result_dict = collections.defaultdict(dict)
-    change_symbol = {ADDED: '+', DELETED: '-', NO_CHANGED: ' ', CHANGED: ''}
     for param in text_list:
         path, operation, value = param
         path_split = path.split('.')
-        path_split[-1] = '{} {}'.format(change_symbol[operation], path_split[-1])
+        path_split[-1] = '{} {}'.format(CHANGE_SYMBOL[operation], path_split[-1])
         result_dict = dictionary_constructor(path_split, value, result_dict)
     result_list = ['{']
     result_list = text_render_print(result_dict, result_list, 1)
@@ -29,9 +34,8 @@ def text_render_print(dictionary, result_list, indent=0):
             text_render_print(value, result_list, indent + 2)
             result_list.append('  ' * indent + '}')
         elif isinstance(value, tuple):
-            for change_value, sign in zip([value[0], value[1]], ['-', '+']):
-                result_list.append('{}{} {}: {}'.format('  ' * indent,
-                                                        sign, key[1:], change_value))
+            result_list.append('{}{} {}: {}'.format('  ' * indent, '-', key[1:], value[0]))
+            result_list.append('{}{} {}: {}'.format('  ' * indent, '+', key[1:], value[1]))
         else:
             result_list.append('{}{}: {}'.format('  ' * indent, str(key), str(value)))
     return result_list
